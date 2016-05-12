044b18585d1987071c2feeda5b5f4d6b0f4d7ea0
Adam Shimali <adam.shimali@gmail.com>
2016-05-12 17:25:24 +0100

    Comment out actually test run for the moment

--- smoke_test_runnner.py
+++ smoke_test_runnner.py
@@ -1,28 +1,31 @@
 #!/usr/bin/env python
 
-import os
-import json
-import requests
+# import os
+# import json
+# import requests
 
 
-data = {"request":
-        {"branch": "master",
-         "config":
-            {"env":
-                {"global": ["TEST_SUITE=smoketest"]},
-                "script": "./scripts/run_smoke_tests.sh"
-             }
-         }
-        }
+# data = {"request":
+#         {"branch": "master",
+#          "config":
+#             {"env":
+#                 {"global": ["TEST_SUITE=smoketest"]},
+#                 "script": "./scripts/run_smoke_tests.sh"
+#              }
+#          }
+#         }
 
 
-headers = {'Content-Type': 'application/json',
-           'Accept': 'application/json',
-           'Travis-API-Version': '3',
-           'Authorization': 'token ' + os.environ['TRAVIS_AUTH_TOKEN']
-           }
-url = "https://api.travis-ci.org/repo/alphagov%2Fnotifications-functional-tests/requests"
-resp = requests.post(url, data=json.dumps(data), headers=headers)
+# headers = {'Content-Type': 'application/json',
+#            'Accept': 'application/json',
+#            'Travis-API-Version': '3',
+#            'Authorization': 'token ' + os.environ['TRAVIS_AUTH_TOKEN']
+#            }
+# url = "https://api.travis-ci.org/repo/alphagov%2Fnotifications-functional-tests/requests"
+# resp = requests.post(url, data=json.dumps(data), headers=headers)
 
-print(resp.status_code)
-print(resp.json())
+# print(resp.status_code)
+# print(resp.json())
+
+
+print('Hook this up to a real job')
